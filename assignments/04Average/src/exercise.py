9216e7eb6555ffcffab5179ac2599f72ddc70410
gilecheverria <gilbertoef@gmail.com>
2021-06-23 15:23:45 -0500

    Remove solution code

--- assignments/04Average/src/exercise.py
+++ assignments/04Average/src/exercise.py
@@ -1,11 +1,7 @@
 def main():
     #escribe tu código abajo de esta línea
-    num1 = int(input("Dame un número: "))
-    num2 = int(input("Dame un número: "))
-    num3 = int(input("Dame un número: "))
+    pass
 
-    promedio = (num1 + num2 + num3) / 3
-    print(f"El promedio de los números es: {promedio}")
 
 if __name__ == '__main__':
     main()
