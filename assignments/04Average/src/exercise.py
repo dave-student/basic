81ea9590a6a576b6945dfe50dfabbb1f01526591
Octavio Navarro <octavio.navarro@tec.mx>
2021-06-23 15:13:48 -0500

    Agregué ejercicio de promedios

---
+++ assignments/04Average/src/exercise.py
@@ -0,0 +1,11 @@
+def main():
+    #escribe tu código abajo de esta línea
+    num1 = int(input("Dame un número: "))
+    num2 = int(input("Dame un número: "))
+    num3 = int(input("Dame un número: "))
+
+    promedio = (num1 + num2 + num3) / 3
+    print(f"El promedio de los números es: {promedio}")
+
+if __name__ == '__main__':
+    main()
