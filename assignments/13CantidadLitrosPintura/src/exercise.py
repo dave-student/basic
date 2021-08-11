3f0b902f470919e1cdb3450309cc9e5015cb2e5e
dcbalderas <dc.balderassilva@tec.mx>
2021-08-11 15:57:26 -0500

    Update exercise.py

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,12 +1,8 @@
-import math
 def main():
     #escribe tu código abajo de esta línea
-
-
-    mts=float(input("Area a pintar en metros: "))
-    rendimiento=float(input("Rendimiento (m2/l): "))
-    litros=math.ceil(mts / rendimiento)
-    print(litros)
+    #Desarrolla un programa en Python que calcule la distancia entre dos puntos del plano cartesiano.
+    #Los mensajes para recibir las 4 entradas deben ser **"x1=    y1=     x2=   y2=   ** respectivamente "
+    #El mensaje para la salida debe ser **"distancia= **"
 
 
 if __name__ == '__main__':
