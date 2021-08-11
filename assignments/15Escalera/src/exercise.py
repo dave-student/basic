b0590f293abc454ccb15c1b43ef9cd25c6450fad
dcbalderas <dc.balderassilva@tec.mx>
2021-08-11 15:58:10 -0500

    Update exercise.py

--- assignments/15Escalera/src/exercise.py
+++ assignments/15Escalera/src/exercise.py
@@ -1,11 +1,8 @@
-import math
 def main():
-    #escribe tu código abajo de esta línea
-    altura=float(input("Altura de la casa: "))
-    angulo=int(input("Angulo en grados: "))
-    radianes=math.radians(angulo)
-    largo=round(altura/math.sin(radianes))
-    print(f"Largo de la escalera: {largo}")
+    # escribe tu código abajo de esta línea
+    # Escribe un programa que determine el largo que debe tener una escalera, la cual se necesita para alcanzar una altura determinada, cuando se pone contra una casa. La altura que se desea alcanzar y el ángulo que deberá hacer la escalera contra la pared, son datos que te proporciona el usuario. 
+    # Para calcular el largo de la escalera usa la siguiente ecuación: largo=altura/seno(ángulo). Recuerda que las funciones de Python que calculan seno y coseno, reciben los ángulos en radianes, pero tu programa va a recibir los ángulos en grados."
+
 
 if __name__ == '__main__':
     main()
